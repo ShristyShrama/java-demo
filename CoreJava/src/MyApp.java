e52cdaf6c4a40659217503e7b97238bfca7b1758
Shristy Sharma <shristysharma222@gmail.com>
2024-03-12 16:04:55 +0530

    base code

---
+++ CoreJava/src/MyApp.java
@@ -0,0 +1,14 @@
+
+public class MyApp
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java Session!!");
+		System.out.println("Be Excpetional");
+		//syso + ctrl + spacebar to write system.out.println 
+		//System - built-in class
+		//out - object for outputStream
+		//println - method to print on console
+		//Note: Byte code or .class file will save in "bin" folder which is not visible in eclipse
+	}
+}
